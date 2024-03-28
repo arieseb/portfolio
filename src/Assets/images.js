941abb225d06b4300967caebab21a2593d11ad93
Sébastien Thomas-Girard <arieseb@gmail.com>
2024-03-28 14:16:29 +0100

    Fix Mahjong SPA screenshot

--- src/Assets/images.js
+++ src/Assets/images.js
@@ -7,7 +7,7 @@ import quaiAntiqueScreen from '../Assets/quai-antique_screen.png';
 import spyScreen from '../Assets/spy_screen.png';
 import folioScreen from '../Assets/folio_screen.png'
 import tattooScreen from '../Assets/tattoo_screen.png'
-import mahjongScreen from '../Assets/tattoo_screen.png'
+import mahjongScreen from '../Assets/mahjong_screen.png'
 
 const images = {
   cvScreen,
